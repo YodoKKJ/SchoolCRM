28dbff093c7ea4a9cf64777ae5530ce0a7b896b4
Yodo <beduschibrunocontato@gmail.com>
2026-02-21 13:09:49 -0300

    first commit

---
+++ src/test/java/com/dom/schoolcrm/SchoolCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dom.schoolcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
